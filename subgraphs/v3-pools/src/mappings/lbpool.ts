3cc531e1c4bc34e8c0dadc2d1be0e797bc437615
gerg <gerrrrrrrg@gmail.com>
2024-11-25 21:13:06 -0500

    add handlers for GradualWeightUpdateScheduled and SwapEnabledSet events

--- subgraphs/v3-pools/src/mappings/lbpool.ts
+++ subgraphs/v3-pools/src/mappings/lbpool.ts
@@ -43,3 +43,31 @@ export function handleLBPoolCreated(event: PoolCreated): void {
     "lbpParams"
   );
 }
+
+export function handleGradualWeightUpdateScheduled(event: GradualWeightUpdateScheduled): void {
+  let poolAddress = event.address;
+  let lbpParams = LbpParams.load(poolAddress);
+
+  if (lbpParams) {
+    lbpParams.startTime = event.params.startTime;
+    lbpParams.endTime = event.params.endTime;
+    lbpParams.startWeights = event.params.startWeights.map<BigDecimal>((weight) =>
+      scaleDown(weight, 18)
+    );
+    lbpParams.endWeights = event.params.endWeights.map<BigDecimal>((weight) =>
+      scaleDown(weight, 18)
+    );
+
+    lbpParams.save();
+  }
+}
+
+export function handleSwapEnabledSet(event: SwapEnabledSet): void {
+  let poolAddress = event.address;
+  let lbpParams = LbpParams.load(poolAddress);
+
+  if (lbpParams) {
+    lbpParams.swapEnabled = event.params.swapEnabled;
+    lbpParams.save();
+  }
+}
